4d133170bccfca200a2aedf6d58b08d32ea0db7e
tomsep <52826851+tomsep@users.noreply.github.com>
2024-01-02 21:35:46 +0200

    Implemented Volume set/get Godot Android plugin

--- plugin/src/main/java/tomsep/snookerclock/SnookerShootoutClock.kt
+++ plugin/src/main/java/tomsep/snookerclock/SnookerShootoutClock.kt
@@ -1,25 +1,34 @@
 package tomsep.snookerclock
 
-import android.util.Log
-import android.widget.Toast
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
+import android.content.Context
+import android.media.AudioManager
 
 class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
 
     override fun getPluginName() = BuildConfig.GODOT_PLUGIN_NAME
 
-    /**
-     * Example showing how to declare a method that's used by Godot.
-     *
-     * Shows a 'Hello World' toast.
-     */
     @UsedByGodot
-    private fun helloWorld() {
-        runOnUiThread {
-            Toast.makeText(activity, "Hello World", Toast.LENGTH_LONG).show()
-            Log.v(pluginName, "Hello World")
-        }
+    private fun getVolume(): Int {
+        val context = activity!!.applicationContext
+        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        return audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+    }
+
+    @UsedByGodot
+    private fun setVolume(volume: Int) {
+        val context = activity!!.applicationContext
+        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        // Get the maximum volume level for music stream
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+
+        // Ensure the volume value is within the valid range
+        val validVolume = volume.coerceIn(0, maxVolume)
+
+        // Set the volume level for music stream
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, validVolume, 0)
     }
 }
