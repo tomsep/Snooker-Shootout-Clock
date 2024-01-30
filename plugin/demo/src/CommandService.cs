c9d659529ab48c05a36cfef9a96cf1acac3eb4da
tomsep <52826851+tomsep@users.noreply.github.com>
2024-01-30 17:46:33 +0200

    Allowed volume changes when app not in focus
    The shot timer is not triggered and new reference volume is set.

--- plugin/demo/src/CommandService.cs
+++ plugin/demo/src/CommandService.cs
@@ -5,6 +5,7 @@ using System;
 public partial class CommandService : Node
 {
     private int _referenceVolume = 0;
+    private DateTime _lastFrameTimestamp;
     private int _count = 0;
     private Node _volumeService;
     private IEventAggregator _events;
@@ -14,18 +15,31 @@ public partial class CommandService : Node
         _events = EventAggregatorService.EventAggregator();
         _volumeService = GetNode("/root/AndroidVolumeService");
         _referenceVolume = GetVolume();
+        _lastFrameTimestamp = DateTime.UtcNow;
     }
 
     public override void _Process(double delta)
     {
         // Publish event if volume was changed outside this application
         // i.e. remote controller changed or hardware volume button.
+
+        // BUT only if app wasn't paused when the volume changed
+        var timeSinceLastFrame = (DateTime.UtcNow - _lastFrameTimestamp).TotalSeconds;
         int volume = GetVolume();
-        if (volume != _referenceVolume)
+        if (volume != _referenceVolume && timeSinceLastFrame <= 1)
         {
             SetVolume(_referenceVolume);
             _events.GetEvent<ShortResetEvent>().Publish();
         }
+        else if (timeSinceLastFrame > 1)
+        {
+            // App was likely paused, so set new reference volume
+            SetVolume(GetVolume());
+            _events.GetEvent<ReferenceVolumeChangedEvent>().Publish();
+            GD.Print("Set new reference volume");
+        }
+
+        _lastFrameTimestamp = DateTime.UtcNow;
     }
 
     public int GetVolume()
@@ -56,3 +70,5 @@ public partial class CommandService : Node
 }
 
 public class ShortResetEvent : PubSubEvent { }
+
+public class ReferenceVolumeChangedEvent : PubSubEvent { }
--- plugin/demo/src/VolumeSlider.cs
+++ plugin/demo/src/VolumeSlider.cs
@@ -1,19 +1,31 @@
 using Godot;
+using Prism.Events;
 using System;
 
 public partial class VolumeSlider : HSlider
 {
     private CommandService _commandService;
+    private IEventAggregator _events;
+
     public override void _Ready()
 	{
         _commandService = GetNode<CommandService>("/root/CommandService");
+        _events = EventAggregatorService.EventAggregator();
+        _events.GetEvent<ReferenceVolumeChangedEvent>().Subscribe(
+            UpdateSliderWithoutNotify, ThreadOption.PublisherThread, false);
         MaxValue = _commandService.GetMaxVolume() - 1;
         Value = _commandService.GetVolume();
         ValueChanged += VolumeSlider_ValueChanged;
     }
 
+
     private void VolumeSlider_ValueChanged(double value)
     {
         _commandService.SetVolume((int)value);
+    }
+
+    private void UpdateSliderWithoutNotify()
+    {
+        SetValueNoSignal(_commandService.GetVolume());
     }
 }
